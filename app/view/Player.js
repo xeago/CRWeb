fc249bfbed47f9391a99b4ad87cfb7ceda9be62c
Ferdinant <LarsBrand@gmail.com>
2012-06-08 15:04:48 +0200

    Clean up: Code

--- app/view/Player.js
+++ app/view/Player.js
@@ -10,39 +10,24 @@ var sponsorPanel  = Ext.create('Ext.Carousel', {
 
     items : [
 		{
-		
-		html: '<img style="height:100%;" src="https://www.dropbox.com/s/xuy6xslukyhfm77/nsfw.jpg?dl=1"></img>',
-			/*xtype:'image',
-			src: 'https://www.dropbox.com/s/xuy6xslukyhfm77/nsfw.jpg?dl=1',
-			cls: 'sponsor-img'*/
+			html: '<img style="height:100%;" src="https://www.dropbox.com/s/xuy6xslukyhfm77/nsfw.jpg?dl=1"></img>',
 			style :'text-align:center;',
 			cls:'card'
-			
 		},
 		{
-		
-		html: '<img style="height:100%;" src="https://www.dropbox.com/s/p94e345k8je513k/sfw.jpg?dl=1"></img>',
-			/*xtype:'image',
-			src: 'https://www.dropbox.com/s/xuy6xslukyhfm77/nsfw.jpg?dl=1',
-			cls: 'sponsor-img'*/
+			html: '<img style="height:100%;" src="https://www.dropbox.com/s/p94e345k8je513k/sfw.jpg?dl=1"></img>',
 			style :'text-align:center;',
 			cls:'card'
-			
 		},
 		{
-		
-		html: '<img style="height:100%;" src="https://www.dropbox.com/s/cosjve0emjwbzw7/sfw2.jpg?dl=1"></img>',
-			/*xtype:'image',
-			src: 'https://www.dropbox.com/s/xuy6xslukyhfm77/nsfw.jpg?dl=1',
-			cls: 'sponsor-img'*/
+			html: '<img style="height:100%;" src="https://www.dropbox.com/s/cosjve0emjwbzw7/sfw2.jpg?dl=1"></img>',
 			style :'text-align:center;',
 			cls:'card'
-			
 		}
 		
 	]
 });
-/*sponsorPanel.setActiveItem(0);*/
+
 
 
 var playing=false;
@@ -72,7 +57,7 @@ var button = Ext.create('Ext.Button', {
 	});
 button.setPressedCls("playerButton-button-pressing");
 button.setHeight(Ext.Viewport.getWindowHeight()*0.6);
-		
+
 var playerPanel = Ext.create('Ext.Container', {
     
     layout: 'hbox',
@@ -88,12 +73,9 @@ var playerPanel = Ext.create('Ext.Container', {
 
 Ext.define("CRWeb.view.Player", {
     extend: 'Ext.Panel',
-	xtype: 'Player',
-	
-		
+	xtype: 'Player',	
     config: {
 		items:[playerPanel]
-		//html: '<div>tekst</div><audio src="http://50.7.241.10:8021/" autoplay="" />'
     }
 });
 
@@ -102,18 +84,12 @@ Ext.define("CRWeb.view.Player", {
                     
 function rotateCarousel()
 {
-	
-	
-	
 	var next =sponsorPanel.getActiveIndex( )+1;
-	//alert(sponsorPanel.getItems( ).length);
 	if(!(next <sponsorPanel.getItems( ).length-1))
 	{
 		next=0;
 	}
-	//alert('rezet');
-	 sponsorPanel.setActiveItem(next);
-	
+	 sponsorPanel.setActiveItem(next);	
 }
 
 setInterval(function(){rotateCarousel()},10000);
